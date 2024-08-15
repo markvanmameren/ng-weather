620abee561a1b5b16cf03ffc8c1ee3aaea6a34da
Mark van Mameren <mark.van.mameren@divotion.com>
2024-08-15 12:57:51 +0200

    test fix

--- src/app/components/tabs/tabs.component.spec.ts
+++ src/app/components/tabs/tabs.component.spec.ts
@@ -1,17 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TabsComponent } from './tabs.component';
 
+@Component({
+  standalone: true,
+  template: ''
+})
+class TestComponent {}
+
 describe('TabsComponent', () => {
-  let component: TabsComponent;
-  let fixture: ComponentFixture<TabsComponent>;
+  let component: TabsComponent<typeof TestComponent>;
+  let fixture: ComponentFixture<TabsComponent<typeof TestComponent>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TabsComponent]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TabsComponent);
+    fixture = TestBed.createComponent(TabsComponent<typeof TestComponent>);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
